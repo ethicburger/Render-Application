fae1f7260239996653babc0eee9cc3a51975717a
ethicburger <ethicburger@gmail.com>
2026-05-16 04:28:27 -0700

    Alter gunicorn for waitress

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask
 
 from routes.main import main
 
-def create_app():
-    app = Flask(__name__)
-    
-    app.register_blueprint(main)
 
-    return app
+app = Flask(__name__)
+    
+app.register_blueprint(main)
